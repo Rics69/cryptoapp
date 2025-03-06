c6f77d8e6cdb506b3bac7084998a484c0d4617b9
Rics <danil.klementev.29@gmail.com>
2025-03-07 00:08:44 +0300

    feat: add statistics component to card

--- src/components/layout/AppSider.tsx
+++ src/components/layout/AppSider.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import {Card, Layout} from "antd";
+import {Card, Layout, Statistic} from "antd";
+import { ArrowUpOutlined } from '@ant-design/icons';
 
 const siderStyle: React.CSSProperties = {
     padding: '1rem'
@@ -9,7 +10,12 @@ const AppSider: React.FC = () => {
     return (
         <Layout.Sider width="25%" style={siderStyle}>
             <Card>
-
+                <Statistic title="Active"
+                           value={11.28}
+                           precision={2}
+                           valueStyle={{color: '#3f8600'}}
+                           prefix={<ArrowUpOutlined/>}
+                           suffix="%"/>
             </Card>
         </Layout.Sider>
     )
