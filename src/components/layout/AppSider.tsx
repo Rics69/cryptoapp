f144e159b0cdd1f3cdbf4a9c45b39f543227a579
Rics <danil.klementev.29@gmail.com>
2025-03-09 16:28:39 +0300

    feat: add assets card

--- src/components/layout/AppSider.tsx
+++ src/components/layout/AppSider.tsx
@@ -88,33 +88,35 @@ const AppSider: React.FC = () => {
 
     return (
         <Layout.Sider width="25%" style={siderStyle}>
-            <Card style={{marginBottom: '1rem'}}>
-                <Statistic title="Active"
-                           value={11.28}
-                           precision={2}
-                           valueStyle={{color: '#3f8600'}}
-                           prefix={<ArrowDownOutlined/>}
-                           suffix="%"/>
-                <List
-                    size='small'
-                    dataSource={data}
-                    renderItem={(item) => (
-                        <List.Item>
-                            <Typography.Text mark>[ITEM]</Typography.Text> {item}
-                        </List.Item>
-                    )}
-                />
-            </Card>
-            <Card>
-                <Statistic
-                    title="Idle"
-                    value={9.3}
-                    precision={2}
-                    valueStyle={{color: '#cf1322'}}
-                    prefix={<ArrowUpOutlined/>}
-                    suffix="%"
-                />
-            </Card>
+            {assets.map(asset => (
+                <Card key={asset.id} style={{marginBottom: '1rem'}}>
+                    <Statistic title="Active"
+                               value={11.28}
+                               precision={2}
+                               valueStyle={{color: '#3f8600'}}
+                               prefix={<ArrowUpOutlined/>}
+                               suffix="%"/>
+                    <List
+                        size='small'
+                        dataSource={data}
+                        renderItem={(item) => (
+                            <List.Item>
+                                <Typography.Text mark>[ITEM]</Typography.Text> {item}
+                            </List.Item>
+                        )}
+                    />
+                </Card>
+            ))}
+            {/*<Card>*/}
+            {/*    <Statistic*/}
+            {/*        title="Idle"*/}
+            {/*        value={9.3}*/}
+            {/*        precision={2}*/}
+            {/*        valueStyle={{color: '#cf1322'}}*/}
+            {/*        prefix={<ArrowDownOutlined/>}*/}
+            {/*        suffix="%"*/}
+            {/*    />*/}
+            {/*</Card>*/}
         </Layout.Sider>
     )
 }
