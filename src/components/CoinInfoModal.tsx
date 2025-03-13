60ad45099f5682d6a9e47a8f77ffa4badd1a2b41
Rics <danil.klementev.29@gmail.com>
2025-03-13 20:53:11 +0300

    feat: add coin info modal header

--- src/components/CoinInfoModal.tsx
+++ src/components/CoinInfoModal.tsx
@@ -1,11 +1,26 @@
+import {Divider, Flex, Typography} from "antd";
+
 interface CoinProps {
     coin: {
         name: string
+        icon: string
+        symbol: string
     } | null
 }
 
 const CoinInfoModal = ({coin}: CoinProps) => {
-    return <h2>{coin ? coin.name : "Монета не выбрана"}</h2>
+    return (
+        <>
+            <Flex align="center">
+                <img src={coin?.icon} alt={coin?.name} style={{width: 40, marginRight: 10}}/>
+                <Typography.Title level={2} style={{margin: 0}}>({coin?.symbol}) {coin?.name}</Typography.Title>
+            </Flex>
+            <Divider/>
+            <Typography.Paragraph>
+
+            </Typography.Paragraph>
+        </>
+    )
 }
 
 export default CoinInfoModal
