3c0aca96cbbce8d9f20cfbb3d37997e75b84514b
Rics <danil.klementev.29@gmail.com>
2025-03-16 17:49:34 +0300

    feat: add table with data of assets

--- src/components/AssetsTable.tsx
+++ src/components/AssetsTable.tsx
@@ -1,5 +1,52 @@
+import {Table, TableColumnsType, TableProps} from "antd";
+import {useCrypto} from "../context/crypto-context.tsx";
+
+interface DataType {
+    key: React.Key;
+    name: string;
+    price: number;
+    amount: number;
+}
+
+const columns: TableColumnsType<DataType> = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        showSorterTooltip: { target: 'full-header' },
+        sorter: (a, b) => a.name.length - b.name.length,
+        sortDirections: ['descend'],
+    },
+    {
+        title: 'Price, $',
+        dataIndex: 'price',
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => a.price - b.price,
+    },
+    {
+        title: 'Amount',
+        dataIndex: 'amount',
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => a.amount - b.amount,
+    },
+];
+
+
 const AssetsTable = () => {
-    return <div>222</div>
+    const {assets} = useCrypto()
+
+    const data = assets.map(a => ({
+        key: a.id,
+        name: a.name,
+        price: a.price,
+        amount: a.amount
+    }))
+
+    return <Table<DataType>
+        pagination={false}
+        columns={columns}
+        dataSource={data}
+        showSorterTooltip={{ target: 'sorter-icon' }}
+    />
 }
 
 export default AssetsTable
